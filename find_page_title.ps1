faf6d1b98331d4e54fc8d7887d57b431341762ed
Paul Sanders <105543178+shagadellic@users.noreply.github.com>
2022-05-16 12:01:48 +1000

    Add files via upload

---
+++ find_page_title.ps1
@@ -0,0 +1,13 @@
+﻿<#
+
+Sample to find the page title in chrome. 
+One browser window open, one tab
+
+copy/paste result into config.xml 
+
+<Title>put in here</Title>
+
+#>
+
+$title = (Get-Process -Name chrome).MainWindowTitle 
+write-host $title
